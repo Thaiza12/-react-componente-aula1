447b22ebc89c588c4a2f865b1fcd00b050765159
Thaiza12 <87837910+Thaiza12@users.noreply.github.com>
2021-11-16 16:28:37 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./index.css";
+import { Message } from "./Message.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Tudo vai dar certo!</h1>
+  
+      <Message />
+    </div>
+  );
+}
